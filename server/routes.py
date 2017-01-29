96b97f004cef52520cabe31d9913f00e46141def
Jared Rickert <jaredrickert52@gmail.com>
2017-01-29 12:04:00 -0600

    unmess stuffs

--- server/routes.py
+++ server/routes.py
@@ -1,5 +1,9 @@
+from multiprocessing import Process
+from queue import Queue
+
 from whereami.learn import learn
 from whereami.predict import locations, predict
+from whereami.pipeline import LearnLocation
 from flask import (
     render_template,
     request,
@@ -11,17 +15,37 @@ from . import (
 )
 from .models import *
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "POST":
-        text = request.form["text"]
-        post = Post(text)
-        db.session.add(post)
-        db.session.commit()
-    posts = Post.query.order_by(Post.date_posted.asc()).all()
-    return render_template("index.html", posts=posts, config=app.config["PORT"])
+    areas = Area.query.order_by(Area.date_posted.asc()).all()
+    return render_template("index.html", areas=areas, config=app.config["PORT"])
 
 
 @app.route("/room")
-def whereami_predict():
-    return predict()
+def api_whereami_predict():
+    try:
+        name = predict()
+        area = Area(name)
+        tick = AreaTick(area.id)
+        db.session.add(tick)
+        db.session.commit()
+        return name
+    except LearnLocation:
+        return "None"
+
+
+@app.route("/learn", methods=["GET", "POST"])
+def api_learn():
+    name = request.form["areaName"]
+    def func():
+        try:
+            Area.learn(name)
+            area = Area(name)
+            db.session.add(area)
+            db.session.commit()
+        except LearnLocation:
+            pass
+    p = Process(target=func)
+    p.start()
+    return index()
