a30b415c9aa4429a786d52f26608115d8ffdf28c
justenrickert <justenrickert@gmail.com>
2017-01-29 12:05:30 -0600

    not sure...

--- server/routes.py
+++ server/routes.py
@@ -1,5 +1,6 @@
 from whereami.learn import learn
 from whereami.predict import locations, predict
+import subprocess
 from flask import (
     render_template,
     request,
@@ -11,6 +12,9 @@ from . import (
 )
 from .models import *
 
+def talk(sentence):
+    subprocess.call(['./say', sentence])
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -21,7 +25,16 @@ def index():
     posts = Post.query.order_by(Post.date_posted.asc()).all()
     return render_template("index.html", posts=posts, config=app.config["PORT"])
 
-
 @app.route("/room")
 def whereami_predict():
+    prediction = predict()
+    talk('the current room is the '+fake_name_to_real_name(prediction))
     return predict()
+
+def fake_name_to_real_name(fake):
+    return {
+        'coding_spo': 'bedroom',
+        'back': 'bathroom',
+        'stage': 'livingroom',
+        'sleeping_area': 'frontdoor',
+    }[fake]
